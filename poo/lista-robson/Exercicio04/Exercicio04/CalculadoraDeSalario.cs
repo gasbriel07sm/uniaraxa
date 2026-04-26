3abb1cd9bd460a72dca143c0ea6922b103cb2bcb
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-04-26 12:31:22 -0300

    Exercicio 04

---
+++ poo/lista-robson/Exercicio04/Exercicio04/CalculadoraDeSalario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio04
+{
+    internal class CalculadoraDeSalario
+    {
+        private string nomeFuncionario;
+        private double salarioBase;
+        private double salarioFinal;
+
+        public void receberDados()
+        {
+            Console.Write("Digite o nome do funcionário: ");
+            nomeFuncionario = Console.ReadLine();
+
+            Console.Write("Digite o salário base do funcionário: ");
+            salarioBase = double.Parse(Console.ReadLine());
+        }
+
+        public void calcularAumento()
+        {
+            Console.Write("Digite o percentual de aumento: ");
+            double percentual = double.Parse(Console.ReadLine());
+
+            salarioFinal = salarioBase + (salarioBase * percentual / 100);
+        }
+
+        public void calcularDesconto()
+        {
+            Console.Write("Digite o percentual de desconto: ");
+            double percentual = double.Parse(Console.ReadLine());
+
+            salarioFinal = salarioBase - (salarioBase * percentual / 100);
+        }
+
+        public void mostrarSalario()
+        {
+            Console.WriteLine($"O salário final do funcionário {nomeFuncionario} é: {salarioFinal:C}");
+        }
+    }
+}
---
+++ poo/lista-robson/Exercicio04/Exercicio04/Program.cs
@@ -0,0 +1,8 @@
+﻿using Exercicio04;
+
+CalculadoraDeSalario calculadora = new CalculadoraDeSalario();
+calculadora.receberDados();
+calculadora.calcularAumento();
+calculadora.mostrarSalario();
+calculadora.calcularDesconto();
+calculadora.mostrarSalario();
