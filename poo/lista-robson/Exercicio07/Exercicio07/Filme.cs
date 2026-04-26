4d279d4cb7e5a06eb697ddc3f9d7eed5dab38f61
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-04-26 12:41:34 -0300

    Exercicio 07

---
+++ poo/lista-robson/Exercicio07/Exercicio07/Filme.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio07
+{
+    internal class Filme
+    {
+        private string titulo;
+        private string genero;
+        private int duracaoMinutos;
+        private int classificacao;
+
+        public Filme(string titulo, string genero, int duracaoMinutos, int classificacao)
+        {
+            if (titulo == "")
+            {
+                this.titulo = "Não informado";
+            }
+            else
+            {
+                this.titulo = titulo;
+            }
+
+            if (genero == "")
+            {
+                this.genero = "Não informado";
+            }
+            else
+            {
+                this.genero = genero;
+            }
+
+            if (duracaoMinutos <= 0)
+            {
+                this.duracaoMinutos = 1;
+            }
+            else
+            {
+                this.duracaoMinutos = duracaoMinutos;
+            }
+
+            if (classificacao < 0)
+            {
+                this.classificacao = 0;
+            }
+            else
+            {
+                this.classificacao = classificacao;
+            }
+        }
+
+        public void ExibirResumo()
+        {
+            Console.WriteLine("===== RESUMO DO FILME =====");
+            Console.WriteLine($"Título: {titulo}");
+            Console.WriteLine($"Gênero: {genero}");
+            Console.WriteLine($"Duração: {duracaoMinutos} minutos");
+            Console.WriteLine($"Classificação indicativa: {classificacao} anos");
+            Console.WriteLine();
+        }
+
+        public bool EhPermitido(int idadePessoa)
+        {
+            return idadePessoa >= classificacao;
+        }
+
+        public void AlterarDuracao(int novaDuracao)
+        {
+            if (novaDuracao > 0)
+            {
+                duracaoMinutos = novaDuracao;
+            }
+        }
+    }
+}
--- poo/lista-robson/Exercicio07/Exercicio07/Program.cs
+++ poo/lista-robson/Exercicio07/Exercicio07/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Exercicio07;
+
+Filme f1 = new Filme("Interestelar", "Ficção Científica", 169, 12);
+Filme f2 = new Filme("", "", -30, -5);
+
+f1.ExibirResumo();
+f2.ExibirResumo();
+
+Console.WriteLine(f1.EhPermitido(10));
+Console.WriteLine(f1.EhPermitido(15));
+
+f2.AlterarDuracao(120);
+f2.ExibirResumo();
+
+f2.AlterarDuracao(-50);
+f2.ExibirResumo();
