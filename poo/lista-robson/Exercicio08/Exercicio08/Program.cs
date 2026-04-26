c850bbb8617952304826b47134eaa90af8e55010
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-04-26 12:42:43 -0300

    Exercicio 08

--- poo/lista-robson/Exercicio08/Exercicio08/Program.cs
+++ poo/lista-robson/Exercicio08/Exercicio08/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Exercicio08;
+
+ReservaHotel r1 = new ReservaHotel("Carlos", 12);
+ReservaHotel r2 = new ReservaHotel("", -8, 0, -300);
+
+r1.ExibirReserva();
+r2.ExibirReserva();
+
+Console.WriteLine($"Total r1: R$ {r1.CalcularTotal():F2}");
+Console.WriteLine($"Total r2: R$ {r2.CalcularTotal():F2}");
+
+r1.AdicionarDiarias(2);
+r1.ExibirReserva();
+
+r2.CancelarReserva();
+r2.ExibirReserva();
+
+r2.ReativarReserva();
+r2.ExibirReserva();
---
+++ poo/lista-robson/Exercicio08/Exercicio08/ReservaHotel.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio08
+{
+    internal class ReservaHotel
+    {
+        private string nomeHospede;
+        private int numeroQuarto;
+        private int quantidadeDiarias;
+        private double valorDiaria;
+        private string statusReserva;
+
+        public ReservaHotel(string nomeHospede, int numeroQuarto)
+        {
+            if (nomeHospede == "")
+            {
+                this.nomeHospede = "Hóspede não informado";
+            }
+            else
+            {
+                this.nomeHospede = nomeHospede;
+            }
+
+            if (numeroQuarto <= 0)
+            {
+                this.numeroQuarto = 1;
+            }
+            else
+            {
+                this.numeroQuarto = numeroQuarto;
+            }
+
+            quantidadeDiarias = 1;
+            valorDiaria = 50;
+            statusReserva = "Ativa";
+        }
+
+        public ReservaHotel(string nomeHospede, int numeroQuarto, int quantidadeDiarias, double valorDiaria)
+        {
+            if (nomeHospede == "")
+            {
+                this.nomeHospede = "Hóspede não informado";
+            }
+            else
+            {
+                this.nomeHospede = nomeHospede;
+            }
+
+            if (numeroQuarto <= 0)
+            {
+                this.numeroQuarto = 1;
+            }
+            else
+            {
+                this.numeroQuarto = numeroQuarto;
+            }
+
+            if (quantidadeDiarias <= 0)
+            {
+                this.quantidadeDiarias = 1;
+            }
+            else
+            {
+                this.quantidadeDiarias = quantidadeDiarias;
+            }
+
+            if (valorDiaria <= 0)
+            {
+                this.valorDiaria = 50;
+            }
+            else
+            {
+                this.valorDiaria = valorDiaria;
+            }
+
+            statusReserva = "Ativa";
+        }
+
+        public double CalcularTotal()
+        {
+            return quantidadeDiarias * valorDiaria;
+        }
+
+        public void ExibirReserva()
+        {
+            Console.WriteLine("===== RESERVA DO HOTEL =====");
+            Console.WriteLine($"Hóspede: {nomeHospede}");
+            Console.WriteLine($"Quarto: {numeroQuarto}");
+            Console.WriteLine($"Diárias: {quantidadeDiarias}");
+            Console.WriteLine($"Valor da diária: R$ {valorDiaria:F2}");
+            Console.WriteLine($"Total: R$ {CalcularTotal():F2}");
+            Console.WriteLine($"Status: {statusReserva}");
+            Console.WriteLine();
+        }
+
+        public void AdicionarDiarias(int quantidade)
+        {
+            if (quantidade > 0)
+            {
+                quantidadeDiarias += quantidade;
+            }
+        }
+
+        public void CancelarReserva()
+        {
+            statusReserva = "Cancelada";
+        }
+
+        public void ReativarReserva()
+        {
+            statusReserva = "Ativa";
+        }
+    }
+}
